6f100fa8c624d874e64ffe389edfb8532536cd66
JonahBlackmon <142601783+JonahBlackmon@users.noreply.github.com>
2023-10-02 17:34:30 +0000

    created default folder, with default install.sh, as well as designated python folder with install.sh

---
+++ src/default/install.sh
@@ -0,0 +1,8 @@
+apk update
+apk add git
+apk add coreutils
+apk add bash
+apk add openssh
+apk add openss1
+apk add gpg
+/workspaces/AlpinePardusFork/src/python/install.sh
--- src/python/install.sh
+++ src/python/install.sh
@@ -1,8 +1,2 @@
 apk update
-apk add git
-apk add coreutils
-apk add bash
-apk add openssh
-apk add openss1
-apk add gpg
 apk add python3 
